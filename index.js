77fd63161dd9ee2b639477a3d7de1630c2d59281
Luis200410 <162214899+Luis200410@users.noreply.github.com>
2026-01-20 14:26:47 -0500

    finish the index part with the correct functionality i need to add the watchlist and localstorage to continue

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
 const form = document.getElementById('movie-form')
-const movieContainer = document.getElementById('movie-container')
+const search = document.getElementById('movie-title')
+const moviesContainer = document.getElementById('movies')
 const apiKey = "eb42fd7d"
 
 
-form.addEventListener('submit', function(e){
+function firstPull(e){
     e.preventDefault()
-    const search = document.getElementById('movie-title').value
-    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${search}`)
+    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${search.value}&type=movie`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
-            movieContainer.innerHTML =
-            `<div class="movie-card">
-                <h3>${data.Title}</h3>
-                <p>Released: ${data.Released}</p>
-                <p>Genre: ${data.Genre}</p>
-                <p>Director: ${data.Director}</p>
-                <img src="${data.Poster}" alt="Movie Poster">
-            </div>`
-
+            const movies = data.Search
+            movies.forEach(movie => {
+                console.log(movie)
+                fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}&type=movie`)
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        moviesContainer.innerHTML += `
+                            <div class="movie">
+                                <div class="movieImage">
+                                    <img src="${data.Poster}" alt="Movie Poster">
+                                </div>
+                                <div class="details">
+                                    <div class="title-rating">
+                                        <h3>${data.Title}</h3>
+                                        <p>${data.imdbRating}</p>
+                                    </div>
+                                    <div class="info">
+                                        <p>${data.Runtime}</p>
+                                        <p>Genre: ${data.Genre}</p>
+                                        <button class="add-watchlist" id="watchlist"><span></span>Watchlist</button>
+                                    </div>
+                                    <div class="plot">
+                                        <p>${data.Plot}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            `
+                    })
             })
-})
+    })
+
+    
+}
+
+
+form.addEventListener('submit', firstPull)
+
+
+    
+// }
+
+
+
+
+
 
