c59d10a0e951993ffdb7cf0ad0976170f12dc33f
Luis200410 <162214899+Luis200410@users.noreply.github.com>
2026-01-21 10:08:37 -0500

    added some symbols and generated the complete logic using local storage

--- index.js
+++ index.js
@@ -1,56 +1,73 @@
 const form = document.getElementById('movie-form')
 const search = document.getElementById('movie-title')
 const moviesContainer = document.getElementById('movies')
-
 const apiKey = "eb42fd7d"
-let watchlist = []
 
+let watchList = []
 
-function firstPull(e){
+moviesContainer.addEventListener("click", function(e){
+    if (e.target.classList.contains('add-watchlist')){
+        let watchList = JSON.parse(localStorage.getItem('myWatchlist'))
+        const movieWatch = {
+            id: e.target.dataset.imdb,
+            title: e.target.dataset.title,
+            rating: e.target.dataset.rating,
+            image: e.target.dataset.poster,
+            duration: e.target.dataset.runtime,
+            genre: e.target.dataset.genre,
+            resume: e.target.dataset.plot
+        }
+
+        watchList.push(movieWatch)
+        localStorage.setItem('myWatchlist', JSON.stringify(watchList))
+    }
+
+})
+
+async function firstPull(e){
     e.preventDefault()
-    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${search.value}&type=movie`)
-        .then(res => res.json())
-        .then(data => {
-            const movies = data.Search
-            movies.forEach(movie => {
-                console.log(movie)
-                fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}&type=movie`)
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data)
-                        moviesContainer.innerHTML += `
-                            <div class="movie" id="${data.imdbID}">
-                                <div class="movieImage">
-                                    <img src="${data.Poster}" alt="Movie Poster">
-                                </div>
-                                <div class="details">
-                                    <div class="title-rating">
-                                        <h3>${data.Title}</h3>
-                                        <p>${data.imdbRating}</p>
-                                    </div>
-                                    <div class="info">
-                                        <p>${data.Runtime}</p>
-                                        <p>Genre: ${data.Genre}</p>
-                                        <button class="add-watchlist" id="watchlist">Watchlist</button>
-                                    </div>
-                                    <div class="plot">
-                                        <p>${data.Plot}</p>
-                                    </div>
-                                </div>
-                            </div>
-                            `
-
-                            const watchlistBtn = document.getElementById(`watchlist`)
-                            watchlistBtn.addEventListener('click', function(){
-                                console.log('clicked')
-                            })
-                        
-                    })
-            })
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${search.value}&type=movie`)
+    const data = await res.json()
+    data.Search.forEach(async movie => {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`)
+        const movieData = await response.json()
+        renderSearch(movieData)
     })
 
-    
+    moviesContainer.innerHTML = ""
 }
+
 
 form.addEventListener('submit', firstPull)
 
+function renderSearch(movieData){
+    moviesContainer.innerHTML += `
+        <div class="movie" id="${movieData.imdbID}">
+            <div class="movieImage">
+                <img src="${movieData.Poster}" alt="Movie Poster">
+            </div>
+            <div class="details">
+                <div class="title-rating">
+                    <h3>${movieData.Title}</h3>
+                    <p>${movieData.imdbRating}</p>
+                </div>
+                <div class="info" id=""info>
+                    <p>${movieData.Runtime}</p>
+                    <p>Genre: ${movieData.Genre}</p>
+                    <button class="add-watchlist" id="watchlist" type="button"
+                        data-imdb="${movieData.imdbID}"
+                        data-title="${movieData.Title}"
+                        data-poster="${movieData.Poster}"
+                        data-rating="${movieData.imdbRating}"
+                        data-runtime="${movieData.Runtime}"
+                        data-genre="${movieData.Genre}"
+                        data-plot="${movieData.Plot}"
+                        ><i class="fa-solid fa-circle-plus"></i> Watchlist</button>
+                </div>
+                <div class="plot">
+                    <p>${movieData.Plot}</p>
+                </div>
+            </div>
+        </div>
+        `
+}
---
+++ watchlist.js
@@ -0,0 +1,45 @@
+const watchContainer = document.getElementById('movies-watchlist')
+let watchList = JSON.parse(localStorage.getItem('myWatchlist'))
+console.log(watchList)
+
+watchList.forEach(movie => renderWatchlist(movie))
+
+
+function renderWatchlist(movie){
+    watchContainer.innerHTML += `
+    <div class="movie" id="${movie.id}">
+        <div class="movieImage">
+            <img src="${movie.image}" alt="Movie Poster">
+        </div>
+        <div class="details">
+            <div class="title-rating">
+                <h3>${movie.title}</h3>
+                <p>${movie.rating}</p>
+            </div>
+            <div class="info">
+                <p>${movie.duration}</p>
+                <p>Genre: ${movie.genre}</p>
+                <button class="remove-watchlist" data-id="${movie.id}" type"button"><i class="fa-solid fa-trash-can"></i> Remove</button>
+            </div>
+            <div class="plot">
+                <p>${movie.resume}</p>
+            </div>
+        </div>
+    </div>
+`
+
+}
+
+watchContainer.addEventListener('click', removeMovie)
+
+function removeMovie(e){
+    if (e.target.classList.contains('remove-watchlist')){
+        const targetId = e.target.dataset.id
+        let watchList = JSON.parse(localStorage.getItem('myWatchlist'))
+        watchList = watchList.filter(movie => movie.id !== targetId )
+        localStorage.setItem('myWatchlist', JSON.stringify(watchList))
+        watchContainer.innerHTML = ''
+        watchList.forEach(movie => renderWatchlist(movie))
+    }
+}
+
